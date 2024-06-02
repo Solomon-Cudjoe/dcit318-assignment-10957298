0fc2d5cae8e5eaf19ad5b5e869839534509e6ad4
SOLOMON CUDJOE <scudjoe016@st.ug.edu.gh>
2024-06-02 14:02:54 +0000

    Grade calculator

---
+++ Grade_Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml.Serialization;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("The Grade Calculator");
+        Console.WriteLine("Please enter grade from 0 - 100: ");
+
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if(grade >= 80)
+        {
+            Console.WriteLine("B");
+        }else if(grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if(grade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+    }
+}
