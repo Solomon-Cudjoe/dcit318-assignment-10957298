99a2a6ee4570ac5207bfb27fec7bd19f1b12f0fa
SOLOMON CUDJOE <scudjoe016@st.ug.edu.gh>
2024-06-02 16:57:08 +0000

    Added other work files

---
+++ Ticket_Price_Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Price_Calculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ticket Price Calculator");
+
+        Console.WriteLine("Please enter your age: ");
+
+        int age = int.Parse(Console.ReadLine());
+        int ticketPrice = (age >= 65 || age <= 12) ? age * 7 : 10;
+        Console.WriteLine("Ticket Price: GHC" + ticketPrice);
+    }
+}
---
+++ Triange_Type_Identifier/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Triangle_Identifier
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Triangle Type Identifier");
+
+        Console.WriteLine("Enter first side length: ");
+        int firstSide = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter second side length: ");
+        int secondSide = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter third side length: ");
+        int thirdSide = int.Parse(Console.ReadLine());
+
+        if(firstSide == secondSide && secondSide == thirdSide) {
+            Console.WriteLine("Equilateral");
+        }
+        else if(firstSide == thirdSide  || secondSide == thirdSide || firstSide == thirdSide) {
+        Console.WriteLine("Isosceles");
+        }
+        else
+        {
+            Console.WriteLine("Scalene");
+        }
+    }
+}
